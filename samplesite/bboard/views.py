5f1272ea7cb437c731623406c8c22a9c801087ea
DolgihIgor <igor_dolgih@inbox.ru>
2023-06-06 13:47:04 +0700

    2.7 Контроллеры-классы

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.template import loader
 from .models import Bb, Rubric
+from .forms import BbForm
 
 
 def index(request):
@@ -17,4 +20,14 @@ def rubric_bbs(request, rubric_id):
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/rubric_bbs.html', context)
-    # return HttpResponse(f"{rubric_id}")
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/bb_create.html'  # путь к файлу шаблона, создающего страницу с формой
+    form_class = BbForm  # ссылка на класс формы, связанной с моделью
+    success_url = reverse_lazy('index')  # интернет адрес для перенаправления после успешного сохранения данных
+
+    def get_context_data(self, **kwargs):  # переопределили метод, чтобы еще выводились рублики
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
